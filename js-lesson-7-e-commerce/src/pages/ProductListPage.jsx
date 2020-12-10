eaa01c3b3618211fa5721a709b872f96ad77246c
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-10 13:17:30 +0100

    Add fetsh  to ProductListPage.js

--- js-lesson-7-e-commerce/src/pages/ProductListPage.jsx
+++ js-lesson-7-e-commerce/src/pages/ProductListPage.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 export default function ProductListPage() {
+
+  useEffect( () => {
+      const url = 'https://mock-data-api.firebaseio.com/e-commerce/products.json'
+      fetch(url)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [] )
+
   return (
     <div>
       <h2>Product List Page</h2>
