ddec8732b66620559b0e2de33fdba65d2a3baa17
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-10 11:36:02 +0100

    Add pages: ProductListPage and ProductDetailPage.jsx

--- js-lesson-7-e-commerce/src/index.js
+++ js-lesson-7-e-commerce/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ js-lesson-7-e-commerce/src/pages/ProductListPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ProductListPage() {
+  return (
+    <div>
+      <h2>Product List Page</h2>
+    </div>
+  )
+}
