6afc64a17c019a915665d3f81df227057a36cbff
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-10 13:35:30 +0100

    Update render Product List Page

--- js-lesson-7-e-commerce/src/pages/ProductListPage.jsx
+++ js-lesson-7-e-commerce/src/pages/ProductListPage.jsx
@@ -1,17 +1,25 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export default function ProductListPage() {
+  const [imageList, setImageList] = useState(null);
 
   useEffect( () => {
       const url = 'https://mock-data-api.firebaseio.com/e-commerce/products.json'
       fetch(url)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => setImageList(data))
   }, [] )
 
   return (
     <div>
       <h2>Product List Page</h2>
+      {!imageList && <p>Loading</p>}
+      {imageList && Object.entries(imageList).map(imageItem => {
+        const key = imageItem[0]
+        const value = imageItem[1]
+        console.log(value)
+        return <p>Image Item</p>
+      })}
     </div>
   )
 }
