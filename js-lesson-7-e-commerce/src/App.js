8fe86db9ce4c2bd5b8f7aa45235a87f175f10b52
Mariia Paraketsova <paraketsova@gmail.com>
2020-12-10 11:49:17 +0100

    Add Switch and Route to App.js

--- js-lesson-7-e-commerce/src/App.js
+++ js-lesson-7-e-commerce/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-
+import { Switch, Route } from 'react-router-dom'
+import ProductListPage from './pages/ProductListPage';
 
 
 function App() {
   return (
     <div>
       <h1>E-Commerce</h1>
+
+      <Switch>
+
+        <Route path='/'>
+          <ProductListPage />
+        </Route>
+        
+      </Switch>
+
     </div>
   );
 }
